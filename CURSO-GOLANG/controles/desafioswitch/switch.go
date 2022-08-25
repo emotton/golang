5581c87ba6e7c830e4a79083b1d4bad96fc35234
emotton <emotton@gmail.com>
2022-08-25 11:33:15 -0300

    Atualização

---
+++ CURSO-GOLANG/controles/desafioswitch/switch.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func notaPraConceito(n float64) string {
+	switch {
+	case n >= 9 && n <= 10:
+		return "A"
+	case n >= 8 && n < 9:
+		return "B"
+	case n >= 5 && n < 8:
+		return "C"
+	case n >= 3 && n < 5:
+		return "D"
+	default:
+		return "E"
+	}
+}
+
+func main() {
+	fmt.Println(notaPraConceito(7.5))
+}
---
+++ CURSO-GOLANG/controles/for/for.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 1
+	for i <= 10 { // Não tem while em Go
+		fmt.Println(i)
+		i++
+	}
+
+	for i := 0; i <= 20; i += 2 {
+		fmt.Printf("%d ", i)
+	}
+
+	for {
+		// laço infinito
+		fmt.Println("Para sempre ...")
+		time.Sleep(time.Second)
+	}
+}
